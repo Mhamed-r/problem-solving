faacd2fb3d8bd21c2375035bdd891b6cba516936
Mohamed Reda <84291864+Mhamed-r@users.noreply.github.com>
2024-12-18 19:43:56 +0200

    Add files via upload

---
+++ Advlab4/Advlab4/Program.cs
@@ -0,0 +1,190 @@
+﻿namespace Advlab4
+{
+    internal class Program
+    {
+        delegate void mydel(Employee emp);
+        delegate void mydel2(Employee emp);
+
+
+        class Employee
+        {
+
+            int age;
+            public event mydel Acceptance_age;
+            public event mydel2 Retirement_age;
+
+            public int ID { get; set; }
+            public string Name { get; set; }
+             DateOnly Emp_data { get; set; }
+
+          
+               
+
+            
+            public DateOnly Age
+            {
+                
+                set
+                {
+                    age = DateTime.Now.Year-Emp_data.Year;
+
+
+                    if (age >= 60)
+                {
+
+                        age = value.Year;
+                        //Retirement_age?.Invoke(this);
+                        //throw new Exception("Retirement age");
+                        Remove_emp();
+
+                    }
+                    else
+                {
+
+                        age = value.Year; ;
+                }
+            }
+            get
+            {
+                return Emp_data;
+
+            }
+
+
+        }
+            public Employee(int ID, string Name , DateOnly Emp_name)
+            {
+
+                this.ID = ID;
+                //this.Age = Age;
+                this.Name = Name;
+                this.Emp_data = Emp_name;
+
+
+            }
+            public void AddEmployee()
+            {
+                //Console.WriteLine( $"Name:{emp.Name} is added to Club and Social " );
+                Acceptance_age?.Invoke(this);
+
+
+
+            }
+            public override string ToString()
+            {
+                return $"{ID}-{Name}-{Age}";
+            }
+            public void Remove_emp()
+            {
+
+
+                
+
+                Retirement_age?.Invoke(this);
+                // throw new Exception("Retirement age");
+
+
+            }
+
+
+
+
+
+        }
+        class Club
+        {
+
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public Club(int ID = 0, string Name = "")
+            {
+                this.ID = ID;
+                this.Name = Name;
+
+            }
+
+
+            public void Add_Employee_club(Employee emp)
+            {
+
+                Console.WriteLine($"Name:{emp.Name} is added to {this.Name}Club @ {DateTime.Now.ToShortTimeString()}");
+
+
+            }
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+              
+
+
+        }
+        class social_insurance
+        {
+            public social_insurance(int ID = 0, string Name = "")
+            {
+                this.ID = ID;
+                this.Name = Name;
+
+            }
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public void Add_Employee_Social(Employee emp)
+            {
+                //DateTime D = new DateTime();
+               int age = DateTime.Now.Year - emp.Age.Year;
+
+                if (age >= 60)
+                {
+                    Console.WriteLine($"Name:{emp.Name} is Removed of {this.Name}insurance @ {DateTime.Now.ToShortTimeString()}");
+
+
+
+                }
+                else
+                {
+                    Console.WriteLine($"Name:{emp.Name} is added to {this.Name}insurance @ {DateTime.Now.ToShortTimeString()}");
+                }
+
+            }
+            public void Remove_Employee_Social(Employee emp)
+            {
+
+                Console.WriteLine($"Name:{emp.Name} is Removed of {this.Name}insurance @ {DateTime.Now.ToShortTimeString()}");
+
+
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Employee emp = new Employee(1,"Mohamed",new DateOnly(2000,05,11));
+            //emp.Age = 100;
+
+            Console.WriteLine(emp.ToString());
+            Club club1 = new Club(1, "Alahly");
+            social_insurance Social1 = new social_insurance(1, "NOSI");
+            emp.Acceptance_age += club1.Add_Employee_club;
+            emp.Acceptance_age += Social1.Add_Employee_Social;
+            emp.Retirement_age += Social1.Remove_Employee_Social;
+            emp.AddEmployee();
+            //emp.Remove_emp();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
---
+++ Advlab4/part2/Program.cs
@@ -0,0 +1,140 @@
+﻿using System.Text;
+
+namespace part2
+{
+ 
+    
+    internal class Program
+    {
+        #region Answer
+        class Answer
+        {
+            public int Number { get; set; }
+            public string []Answers { get; set; }
+
+
+            //public override string ToString()
+            //{
+            //    S
+
+            //}
+            public Answer(int num, string []Answer)
+            {
+                    this.Number = num;
+                this.Answers = Answer;
+            }
+
+            public override string ToString()
+            {
+
+
+                StringBuilder stringBuilder = new StringBuilder($"{Number}: ");
+                foreach (var item in Answers)
+                {
+                    stringBuilder.Append(item.ToString()+"\t");
+
+
+                }
+
+                return stringBuilder.ToString();
+            }
+
+        }
+
+        class Question
+        {
+            public string Type { get; set; }
+            public int QuestionNumber { get; set; }
+            public string Body { get; set; }
+            public int Mark { get; set; }
+            //public Answer Answers { get; set; }
+
+
+            public Question(int number,string type, string body, int mark)
+            {
+                this.Type = type;
+                this.QuestionNumber = number;
+                this.Body = body;
+                this.Mark = mark;
+            }
+            public override string ToString()
+            {
+               return $"{QuestionNumber}: Type Of Question Is {Type} \t  \t Mark : {Mark} \n {Body} ";
+          
+            }
+        }
+        class AnswerList : List<Answer>
+        {
+            public override string ToString()
+            {
+                string text = "";
+                foreach (Answer item in this)
+                {
+                    text += item.ToString();
+                }
+                return text;
+            }
+
+
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+           
+            List<Answer> Answers = new List<Answer>() {
+                
+          new Answer(1,new string[]{ "Answer_one"}),
+          new Answer(2,new string[]{ "Answer_Two"}),
+          new Answer(3,new string[]{ "Answer_Three"}),
+          new Answer(4,new string[]{ "Answer_Four"})
+
+
+
+            };
+            //Console.WriteLine("Enter Choose");
+            //int Choose = int.Parse(Console.ReadLine());
+            //switch (Choose)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Fasle");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Fasle");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("True");
+            //        break;
+
+            //    case 4:
+            //        Console.WriteLine("Fasle");
+            //        break;
+
+            //    default:
+            //        break;
+            //}
+
+            Dictionary<Question, List<Answer>> dic = new Dictionary<Question, List<Answer>>();
+            dic.Add(new Question(1, "Choose One", "The blue whale is the biggest animal to have ever lived.",10), Answers);
+            dic.Add(new Question(2, "Choose One", "The blue whale is the biggest animal to have ever lived.", 10), Answers);
+            dic.Add(new Question(3, "Choose One", "The blue whale is the biggest animal to have ever lived.", 10), Answers);
+            dic.Add(new Question(4, "Choose One", "The blue whale is the biggest animal to have ever lived.", 10), Answers);
+           foreach(KeyValuePair < Question, List < Answer >> item in dic)
+            {
+                Console.WriteLine(item.Key);
+                foreach (Answer s in item.Value)
+                {
+                    Console.WriteLine(s +"  ");
+                }
+            }
+
+           
+
+
+
+
+
+
+
+        }
+    }
+}
