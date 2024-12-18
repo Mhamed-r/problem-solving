f777d93331ee0ddbcd64cd883c71cbb84a692429
Mohamed Reda <84291864+Mhamed-r@users.noreply.github.com>
2024-12-18 19:42:40 +0200

    Add files via upload

---
+++ Advday3/Advday3/Program.cs
@@ -0,0 +1,192 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Advday3
+{
+    struct hiredate:IComparable<hiredate>
+    {
+        public hiredate(int year, int month, int day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+
+        public int CompareTo(hiredate obj)
+        {
+            //if (obj.Year.CompareTo(Year) == 0) ;
+            //else if (obj.Month.CompareTo(Month) == 0) ;
+            //else { return -1; }
+
+            if (Year > obj.Year || Year == obj.Year && Month > obj.Month || Year == obj.Year && Month == obj.Month && Day > obj.Day)
+            {
+                return 1;
+            }
+            else if (Year < obj.Year || Year == obj.Year && Month < obj.Month || Year == obj.Year && Month == obj.Month && Day < obj.Day) { return -1; }
+            else { return 0; }
+
+            //if (Year > obj.Year) return 1;
+            //else if (Year < obj.Year) return -1;
+            //else if(Month > obj.Month) return 1;
+            //else if(Month < obj.Month) return -1;
+            //else if (Day > obj.Day) return 1;
+            //else if (Day < obj.Day) return -1;
+            //else return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Day}/{Month}/{Year}";
+
+        }
+
+
+    }
+    enum gender
+    {
+        Male, Female
+
+    }
+    [Flags]
+    enum privileges : byte
+    {
+        guest = 1, Developer = 2, secretary = 4, DBA = 8
+
+    }
+    class Employee : IComparable<Employee>
+    {
+
+        public Employee()
+        {
+        }
+        public Employee(int ID, gender Gender, int Salary, hiredate hiredate, privileges securitylevel)
+        {
+            this.securitylevel = securitylevel;
+            this.ID = ID;
+            this.Gender = Gender;
+            this.Salary = Salary;
+            this.hiredate = hiredate;
+        }
+
+        public int ID { get; set; }
+        public gender Gender { get; set; }
+        public int Salary { get; set; }
+        public privileges securitylevel { get; set; }
+        public hiredate hiredate { get; set; }
+
+        public int CompareTo(Employee? other)
+        {
+          return(hiredate.CompareTo(other.hiredate));
+        }
+
+        public override string ToString()
+        {
+
+            return $"Employee ID:{ID}\nEmployee Salary:{Salary:C}\nGender:{Gender}\nhiredate:{hiredate.ToString()}\nsecuritylevel:{securitylevel}\n";
+
+        }
+
+
+    }
+
+
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee emp = new Employee(15,gender.Male,5000,new hiredate(2005,11,12),privileges.DBA );
+            Employee emp1 = new Employee(15, gender.Male, 5000, new hiredate(1999, 11, 12),  privileges.guest);
+            Employee emp2 = new Employee(15, gender.Male, 5000, new hiredate(2005, 11, 5),privileges.secretary);
+            Employee[] EmpArr = new Employee[] {emp, emp1, emp2 };
+            Array.Sort(EmpArr);
+            for (int i = 0; i < EmpArr.Length; i++)
+            {
+                Console.WriteLine(EmpArr[i].ToString());
+            }
+
+            // Allow NO RUNTIME errors if the user inputs any data
+            #region user_input
+            try
+            {
+                Console.WriteLine("Enter numbers of employees");
+                int Numberofemployee = int.Parse(Console.ReadLine());
+                Employee[] Employee1 = new Employee[Numberofemployee];
+                for (int i = 0; i < Employee1.Length; i++)
+                {
+
+                    Console.WriteLine($"\nEnter EmployeeID {i + 1}:");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"\nEnter EmployeeGender {i + 1}:");
+                 
+                    string X=Console.ReadLine();
+                    gender g = (gender)Enum.Parse(typeof(gender), X);
+                  
+
+                    Console.WriteLine($"\nEnter EmployeeSalary {i + 1}:");
+                    int Salary = int.Parse(Console.ReadLine());
+                    
+                    Console.WriteLine($"\nEnter securitylevel {i + 1}:");
+                    string Z = Console.ReadLine();
+                    privileges u = (privileges)Enum.Parse(typeof(privileges),Z);
+
+                    Console.WriteLine($"\nEnter hiredate {i + 1}:");
+                    Console.Write("Year: ");
+                    int Years = int.Parse(Console.ReadLine());
+                    Console.Write("Month: ");
+                    int Months = int.Parse(Console.ReadLine());
+                    Console.Write("Day: ");
+                    int Days = int.Parse(Console.ReadLine());
+                    Employee1[i] = new Employee
+                    {
+                        ID = id,
+                        Gender = g,
+                        Salary = Salary,
+                        securitylevel = u,
+
+
+                        // hiredate = new hiredate { Year = Years, Month = Months, Day = Days }
+                        hiredate = new hiredate(Years, Months, Days)
+
+
+
+                    };
+                    Console.WriteLine($"Before Sorting");
+                    foreach (Employee Employee in Employee1)
+                    {
+
+                        Console.WriteLine(Employee.ToString());
+                    }
+                    Array.Sort(Employee1);
+                    Console.WriteLine($"After Sorting");
+                    foreach (Employee Employee in Employee1)
+                    {
+
+                        Console.WriteLine(Employee.ToString());
+                    }
+
+                }
+
+
+
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+
+
+            #endregion
+
+
+
+
+
+            //Console.WriteLine(emp.ToString());
+
+
+        }
+
+    }
+}
+
---
+++ Advday3/Advpart2/Program.cs
@@ -0,0 +1,169 @@
+﻿
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+
+namespace Advpart2
+{
+
+    //public delegate T fPtr<T, T1>(T1 x);
+    public delegate string fPtr(Book x);
+
+    public class Book
+    {
+        public Book() { }
+
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string _ISBN, string _Title, string[] _Authors, DateTime _PublicationDate, decimal _Price)
+        {
+            this.ISBN = _ISBN;
+            this.Title = _Title;
+            this.Authors = _Authors;
+            this.PublicationDate = _PublicationDate;
+            this.Price = _Price;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder($"{ISBN}-{Title}-{PublicationDate}  {Price.ToString()}$\t");
+            foreach (var item in Authors)
+            {
+                stringBuilder.Append(item.ToString() + "\t");
+            }
+
+            return stringBuilder.ToString();
+        }
+        public  string ToString(Book B)
+        {
+            StringBuilder stringBuilder = new StringBuilder($"{B.ISBN}-{B.Title}-{B.PublicationDate}  {B.Price.ToString()}$\t");
+            foreach (var item in B.Authors)
+            {
+                stringBuilder.Append(item.ToString() + "\t");
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+
+    public class BookFunctions
+    {
+        public static string GetTitle(Book B)
+        {
+            return B.Title;
+        }
+        public static string GetISBN(Book B)
+        {
+            return B.ISBN;
+        }
+        public static string GetDate(Book B)
+        {
+            return B.PublicationDate.ToShortDateString();
+        }
+       
+
+        public static string GetAuthors(Book B)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < B.Authors.Length;i++) 
+                sb.Append(B.Authors[i].ToString());                
+            return sb.ToString();
+        }
+
+        public static string GetPrice(Book B)
+        {
+            return B.Price.ToString()+"$";
+        }
+    }
+
+    public class LibraryEngine
+    {
+        public static void ProcessBooks(List<Book> bList, fPtr Ptr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(Ptr(B));
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Book> B = new List<Book>()
+            {
+                new Book("1", "Book1", new string[] {"Author1"}, DateTime.Now, 500),
+                new Book("2", "Book2", new string[] {"Author2"}, DateTime.Now, 600),
+                new Book("3", "Book3", new string[] {"Author3"}, DateTime.Now, 700),
+                new Book("4", "Book4", new string[] {"Author4"}, DateTime.Now, 800)
+            };
+            //List<Book> LB = new List<Book>() {
+
+            //LB.Add( new Book () );
+
+            //LB.Add(ISBN1),
+            //LB.Add(ISBN2),
+            //LB.Add(ISBN3),
+
+
+
+
+            //};
+            //foreach (Book B in LB)
+            //{
+
+            //    Console.WriteLine("Enter Book ISBN");
+            //    string ISBN = Console.ReadLine();
+
+            //    Console.WriteLine("Enter Book ISBN");
+            //    string ISBN1 = Console.ReadLine();
+
+
+            //    Console.WriteLine("Enter Book ISBN");
+            //    string ISBN2 = Console.ReadLine();
+
+
+            //    Console.WriteLine("Enter Book ISBN");
+            //    string ISBN3 = Console.ReadLine();
+
+
+            //}
+
+            Book M = new Book();
+            fPtr RfPtr = M.ToString;
+            LibraryEngine.ProcessBooks(B, RfPtr);
+            Console.WriteLine("-----------------------------------------------");
+            Func<Book, string> S = BookFunctions.GetISBN;
+            LibraryEngine.ProcessBooks(B, S.Invoke);
+            Console.WriteLine("-------");
+            Func<Book, string> d = delegate (Book y) { return y.Title; };
+            LibraryEngine.ProcessBooks(B, d.Invoke);
+            Console.WriteLine("-------");
+            Func<Book, string> F = delegate (Book B1) { return BookFunctions.GetAuthors(B1); };
+            LibraryEngine.ProcessBooks(B, F.Invoke);
+            Console.WriteLine("-------");
+            fPtr RfPtr2 = BookFunctions.GetDate;
+            LibraryEngine.ProcessBooks(B, RfPtr2);
+            Console.WriteLine("-------");
+            Func<Book, string> W = B2 => (BookFunctions.GetPrice(B2));
+            LibraryEngine.ProcessBooks(B, W.Invoke);
+            
+
+
+
+            //RfPtr.Invoke(B);
+            //  Func<Book, string> W = X => X.ISBN;
+            //  Func <List<Book>, fPtr> W= B1.ProcessBooks;
+            //fPtr<string, Book> RfPtr = BookFunctions.GetTitle;
+            //LibraryEngine.ProcessBooks(B, W);
+            //Func<Book, string> d = delegate (Book y) { return y.ISBN; };
+            //LibraryEngine.ProcessBooks(B, d);
+
+        }
+    }
+}
